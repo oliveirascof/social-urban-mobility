9e36ce4348bf3d92152a3d7bec292387bcd92409
Felipe Oliveira <oliveirascof@gmail.com>
2022-09-22 17:08:29 -0300

    reloaded project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
-import Header from './components/Header';
-import Button from './components/Button/style';
-import { BrowserRouter as Router } from 'react-router-dom';
 
-const App = () => {
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+function Example(props) {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
   return (
-    <Router>
-      <Header />
-    </Router>
-  )
+    <div>
+      <Navbar color="faded" light>
+        <NavbarBrand href="/" className="me-auto">
+          reactstrap
+        </NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="me-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
-export default App;
-
+export default Example;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,28 +1,5 @@
-import React from "react";
+import { Button } from 'reactstrap';
 
-import { NavContainer } from "./style";
-import { NavLogo } from "./style";
-import { NavMenu } from "./style";
-import { Input } from "./style";
-import Button from "../Button/style";
-
-import { Link } from "react-router-dom";
-
-const Header = () => {
-    return(
-        <NavContainer>
-            <NavMenu>
-                Para onde deseja ir?
-            </NavMenu>
-
-        <div>
-            <Input />
-            <Button >Pesquisar</Button>
-        </div>
-
-            
-        </NavContainer>
-    )
+export default (props) => {
+    return <Button color="danger"/>
 }
-
-export default Header;
